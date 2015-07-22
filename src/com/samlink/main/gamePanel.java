d69aaa482f58f0347004d78ef7c59b17c79077ac
walterraftus <walterraftus@inside-177-233.wireless.utoronto.ca>
2015-07-22 11:03:34 -0400

    hi

---
+++ src/com/samlink/main/gamePanel.java
@@ -0,0 +1,58 @@
+package com.samlink.main;
+
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JPanel;
+
+
+public class gamePanel extends JPanel implements KeyListener{
+
+	//character variables
+	int mainCharX = 200;
+	int mainCharY = 200;
+	boolean movingUp = false;
+	
+	gamePanel(){
+		
+	}
+	
+	public void paintComponent (Graphics g){
+		g.drawRect(mainCharX, mainCharY, 20, 20);
+		g.fillRect(mainCharX, mainCharY, 20, 20);
+	}
+	
+	public void run(){
+		
+		if(movingUp){
+			mainCharY += 5;
+		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		switch(e.getKeyChar()){
+		case 'w':
+				movingUp = true;
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		switch(e.getKeyChar()){
+		case 'w':
+				movingUp = false;
+		}
+	}
+	
+}
