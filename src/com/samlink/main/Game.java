eaf15ae0e23d956e6b30793c1744ea9d34cb1550
walterraftus <walterraftus@inside-177-233.wireless.utoronto.ca>
2015-07-22 11:03:03 -0400

    ha

--- src/com/samlink/main/Game.java
+++ src/com/samlink/main/Game.java
@@ -1,10 +1,45 @@
 package com.samlink.main;
 
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+
 public class Game {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
+		//JFrame Setup
+		JFrame myFrame = new JFrame();
+		myFrame.setVisible(true);
+		myFrame.setSize(640, 480);
+		myFrame.setResizable(false);
+		myFrame.setTitle("DLC");
+		myFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		
+		//JPanel Setup
+		gamePanel myPanel = new gamePanel();
+		myPanel.setSize(640, 480);
+		myPanel.setBackground(Color.WHITE);
+		myFrame.setContentPane(myPanel);
+		myPanel.requestFocus();
+		
+		//myPanel.addKeyListener(myPanel);
+		//myPanel.addMouseMotionListener(myPanel);
+		//myPanel.addMouseListener(myPanel);
+		
+		while (true) {
+
+			myPanel.run();
+			myPanel.repaint();
+
+			Thread.sleep(17);
+
+
+		}
 	}
 
 }
